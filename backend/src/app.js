b41e7de241a55bbc555e8cace5a1f830b2763e14
Brobo1 <armanahz95@gmail.com>
2025-01-30 13:38:05 +0100

    Add user and message data with expanded user routes
    Introduced in-memory user and message objects for dynamic data handling. Updated `/users` route to return user data and added new endpoints for fetching, updating, and deleting specific users. Enhanced `requests.http` with detailed examples for all user-related HTTP methods.

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,30 @@ import express from "express";
 
 let app = express();
 
+let users = {
+  1: {
+    id: "1",
+    username: "Robin Wieruch",
+  },
+  2: {
+    id: "2",
+    username: "Dave Davids",
+  },
+};
+
+let messages = {
+  1: {
+    id: "1",
+    text: "Hello World",
+    userId: "1",
+  },
+  2: {
+    id: "2",
+    text: "By World",
+    userId: "2",
+  },
+};
+
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -20,13 +44,16 @@ app.delete("/", (req, res) => {
 });
 
 app.get("/users", (req, res) => {
-  return res.send("GET HTTP method on user resource");
+  return res.send(Object.values(users));
+});
+app.get("/users/:userId", (req, res) => {
+  return res.send(users[req.params.userId]);
 });
 app.post("/users", (req, res) => {
   return res.send("POST HTTP method on user resource");
 });
 app.put("/users/:userId", (req, res) => {
-  return res.send("PUT HTTP method on user resource");
+  return res.send(`PUT HTTP method on user/${req.params.userId}`);
 });
 app.delete("/users/:userId", (req, res) => {
   return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
