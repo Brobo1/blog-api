9771f9a0a0df5adbcf653d4a45c4a65a5477da58
Brobo1 <armanahz95@gmail.com>
2025-02-10 15:33:35 +0100

    ``` refactor(routes): modularize and simplify route handling
    Extracted session, user, and message routes into separate modules and integrated them using `app.use()` for improved maintainability and code organization.
    ```

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import express from "express";
 import { v4 as uuid4 } from "uuid";
 import models from "./models";
+import routes from "./routes/index.js";
 
 let app = express();
 
@@ -16,65 +17,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/", (req, res) => {
-  return res.send("Received a GET HTTP method");
-});
-app.post("/", (req, res) => {
-  return res.send("Received a POST HTTP method");
-});
-app.put("/", (req, res) => {
-  return res.send("Received a PUT HTTP method");
-});
-app.delete("/", (req, res) => {
-  return res.send("Received a DELETE HTTP method");
-});
-
-app.get("/session", (req, res) => {
-  return res.send(req.context.models.users[req.context.me.id]);
-});
-
-app.get("/users", (req, res) => {
-  return res.send(Object.values(req.context.models.users));
-});
-app.get("/users/:userId", (req, res) => {
-  return res.send(req.context.models.users[req.params.userId]);
-});
-app.post("/users", (req, res) => {
-  return res.send("POST HTTP method on user resource");
-});
-app.put("/users/:userId", (req, res) => {
-  return res.send(`PUT HTTP method on user/${req.params.userId}`);
-});
-app.delete("/users/:userId", (req, res) => {
-  return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
-});
-
-app.get("/messages", (req, res) => {
-  return res.send(Object.values(req.context.models.messages));
-});
-app.get("/messages/:messageId", (req, res) => {
-  return res.send(req.context.models.messages[req.params.messageId]);
-});
-app.post("/messages", (req, res) => {
-  const id = uuid4();
-  const message = { id, text: req.body.text, userId: req.context.me.id };
-  req.context.models.messages[id] = message;
-  return res.send(message);
-});
-app.put("/messages/:messageId", (req, res) => {
-  const message = {
-    ...req.context.models.messages[req.params.messageId],
-    text: req.body.text,
-  };
-  req.context.models.messages[req.params.messageId] = message;
-  res.send(message);
-});
-app.delete("/messages/:messageId", (req, res) => {
-  const { [req.params.messageId]: message, ...otherMessages } =
-    req.context.models.messages;
-  req.context.models.messages = otherMessages;
-  return res.send(message);
-});
+app.use("/session", routes.session);
+app.use("/users", routes.user);
+app.use("/message", routes.message);
 
 const PORT = process.env.PORT;
 console.log(PORT);
