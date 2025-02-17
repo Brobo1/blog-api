5936f8e68a5fedb7ed8e651c1da946ce3762921c
Brobo1 <armanahz95@gmail.com>
2025-02-17 19:05:23 +0100

    `feat: add route to fetch comments for a post`

--- backend/src/routes/comment.js
+++ backend/src/routes/comment.js
@@ -1,3 +1,9 @@
 import { Router } from "express";
+import {
+  getCommentController,
+  getCommentsController,
+} from "../controllers/comment.js";
 
 const router = Router();
+
+router.get("/:postId", getCommentsController);
