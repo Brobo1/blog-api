43e502200b6ad2bc8f50fac6e965b197154c2ab4
Brobo1 <armanahz95@gmail.com>
2025-03-18 17:53:04 +0100

    `feat(auth): implement user login with username, password validation, and token generation`

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -1,7 +1,39 @@
 import { Request, Response } from "express";
-import { getUserQuery } from "../queries/user";
+import { getUserByUsernameQuery } from "../queries/user";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const loginController = async (req: Request, res: Response) => {
-  const isUser = await getUserQuery(req.body.userId);
-  res.json(isUser);
+  //CHANGE TO USERNAME WHEN GOING LIVE
+  const isUser = await getUserByUsernameQuery(req.body.username);
+
+  if (!isUser) {
+    res.status(400).json({ message: "User does not exist" });
+    return;
+  }
+
+  const isPasswordMatch = await bcrypt.compare(
+    req.body.password,
+    isUser.password,
+  );
+
+  console.log(isPasswordMatch);
+
+  if (!isPasswordMatch) {
+    res.status(400).json({ message: "Incorrect password" });
+    return;
+  }
+
+  //Generate token
+  const accessToken = jwt.sign(
+    {
+      id: isUser.id,
+    },
+    "secret",
+    {
+      expiresIn: "1d",
+    },
+  );
+
+  res.status(200).json({ message: "User logged in", accessToken });
 };
