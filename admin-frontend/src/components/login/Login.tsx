a27ea6d30a52e53c81b469961bbc2af33a6cbc12
Brobo1 <armanahz95@gmail.com>
2025-03-21 23:27:29 +0100

    `fix(login): apply styling to login form using CSS module`

--- admin-frontend/src/components/login/Login.tsx
+++ admin-frontend/src/components/login/Login.tsx
@@ -1,24 +1,24 @@
-import {loginUser} from "../../api/user.ts";
-import style from "./Login.module.css"
+import { loginUser } from "../../api/user.ts";
+import style from "./Login.module.css";
 
 export function Login() {
-	//TODO Store JWT
-	async function userLogin(formData: FormData) {
-		const {username, password} = Object.fromEntries(formData.entries()) as {
-			username:string;
-			password:string;
-		}
-		const response = await loginUser(username, password)
-		console.log(response);
-	}
-	
-	return (
-		<>
-			<form action={userLogin} style={style} >
-				<input type="text" name="username"/>
-				<input type="text" name="password"/>
-				<button type={"submit"}>Login</button>
-			</form>
-		</>
-	);
+  //TODO Store JWT
+  async function userLogin(formData: FormData) {
+    const { username, password } = Object.fromEntries(formData.entries()) as {
+      username: string;
+      password: string;
+    };
+    const response = await loginUser(username, password);
+    console.log(response);
+  }
+
+  return (
+    <>
+      <form id={style.loginForm} action={userLogin}>
+        <input type="text" name="username" />
+        <input type="text" name="password" />
+        <button type={"submit"}>Login</button>
+      </form>
+    </>
+  );
 }
