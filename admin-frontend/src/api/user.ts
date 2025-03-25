61ed9191c2d4834a48b8693bf8b73b07ba786d12
Brobo1 <armanahz95@gmail.com>
2025-03-25 15:08:02 +0100

    ``` refactor(api): add user parameter to createUser and align fetch implementations ```

--- admin-frontend/src/api/user.ts
+++ admin-frontend/src/api/user.ts
@@ -1,30 +1,37 @@
-import {createUserEndpoint, getUserEndpoint, loginEndpoint} from "../util/urls.ts";
+import {
+  createUserEndpoint,
+  getUserEndpoint,
+  loginEndpoint,
+} from "../util/urls.ts";
+import { User } from "../types/types.ts";
 
-export async function createUser() {
-	await fetch(createUserEndpoint)
+export async function createUser(user: User) {
+  await fetch(createUserEndpoint, {
+    method: "POST",
+    body: JSON.stringify(user),
+  });
 }
 
 export async function getUser(username: string, password: string) {
-	await fetch(getUserEndpoint, {
-		method: "POST",
+  await fetch(getUserEndpoint, {
+    method: "POST",
 
-		body: JSON.stringify({username, password})
-	})
+    body: JSON.stringify({ username, password }),
+  });
 }
 
 export async function loginUser(username: string, password: string) {
-	try {
-		const response = await fetch(loginEndpoint, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify({username, password})
-		})
-		const json = await response.json()
-		return json
-		
-	} catch (err) {
-		console.error("Unable to login", err)
-	}
+  try {
+    const response = await fetch(loginEndpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+    const json = await response.json();
+    return json;
+  } catch (err) {
+    console.error("Unable to login", err);
+  }
 }
