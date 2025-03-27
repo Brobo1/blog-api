f77c4a125a8951b8c4d3d5c90094a2951581d282
Brobo1 <armanahz95@gmail.com>
2025-03-27 17:45:09 +0100

    `refactor: replace login and register components with content component in App`

--- admin-frontend/src/App.tsx
+++ admin-frontend/src/App.tsx
@@ -1,22 +1,12 @@
 import "./App.css";
-import { Login } from "./components/login/Login.tsx";
-import { Register } from "./components/register/Register.tsx";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
+import { Content } from "./components/content/Content.tsx";
 
 function App() {
   return (
     <>
       <AuthProvider>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            gap: "20px",
-          }}
-        >
-          <Login />
-          <Register />
-        </div>
+        <Content />
       </AuthProvider>
     </>
   );
