96fcbef0a44fd6998e5c99cb5851ad9b4405c032
Brobo1 <armanahz95@gmail.com>
2025-04-30 17:59:25 +0200

    ``` refactor: remove auth dependency from Content component ```

--- admin-frontend/src/components/content/Content.tsx
+++ admin-frontend/src/components/content/Content.tsx
@@ -1,22 +1,15 @@
-import { useAuth } from "../../contexts/AuthContext.tsx";
 import { PostForm } from "../forms/PostForm.tsx";
 import { Login } from "../login/Login.tsx";
 
 export function Content() {
-  const { isAuth, logout, user } = useAuth();
   return (
     <>
-      {isAuth ? (
+      {
         <div>
-          <h2>Welcome {user?.username}</h2>
           <PostForm />
-          <button onClick={logout}>Logout</button>
-        </div>
-      ) : (
-        <div>
           <Login />
         </div>
-      )}
+      }
     </>
   );
 }
